50e7ff3a7c1e2b44284edb52bb11c239af451a18
nouredinemoussa <nouredine.moussa1@gmail.com>
2024-11-22 22:27:57 +0100

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
+import { slideInFromLeft, slideInFromTop } from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 
 const SkillText = () => {
